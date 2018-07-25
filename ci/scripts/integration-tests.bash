a40926acd656ba091d4e8129a349bf389e6a5fb2
Kevin Yeap <kyeap@pivotal.io>
2018-07-25 12:05:11 -0700

    integration: run integration test in ci
    Co-authored-by: Kevin Yeap <kyeap@pivotal.io>
    Co-authored-by: Jim Doty <jdoty@pivotal.io>
    Co-authored-by: Nadeem Ghani <nghani@pivotal.io>
    Co-authored-by: Amil Khanzada <akhanzada@pivotal.io>
    Co-authored-by: Kris Macoskey <kmacoskey@pivotal.io>
    Co-authored-by: Jamie McAtamney <jmcatamney@pivotal.io>

---
+++ ci/scripts/integration-tests.bash
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+
+set -ex
+
+# make depend fails if run as gpadmin with a dep ensure git-remote-https signal 11 failure
+GOPATH="$PWD/go" PATH="$PWD/go/bin:$PATH" make -C go/src/github.com/greenplum-db/gpupgrade depend
+
+# build and setup demo cluster
+source gpdb_src/concourse/scripts/common.bash
+time install_gpdb
+time ./gpdb_src/concourse/scripts/setup_gpadmin_user.bash "centos"
+time make_cluster
+
+chown -R gpadmin:gpadmin go
+
+su gpadmin -c 'export GOPATH=$PWD/go && \
+export PATH=$GOPATH/bin:$PATH && \
+source /usr/local/greenplum-db-devel/greenplum_path.sh && \
+source gpdb_src/gpAux/gpdemo/gpdemo-env.sh && \
+make -C $GOPATH/src/github.com/greenplum-db/gpupgrade integration'
+
