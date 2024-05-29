39824351b55bc7a84296bacf369688e6d7c07425
Brent Doil <bdoil@vmware.com>
2024-05-29 17:45:58 -0400

    Use GIT_SSH_KEY to call git fetch

--- ci/main/scripts/build.bash
+++ ci/main/scripts/build.bash
@@ -4,8 +4,14 @@
 
 set -eux -o pipefail
 
+mkdir -p ~/.ssh
+ssh-keyscan github.com > ~/.ssh/known_hosts
+echo "${GIT_SSH_KEY}" > ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
+
 cd gpupgrade_src
 export GOFLAGS="-mod=readonly" # do not update dependencies during build
+git fetch git@github.com:greenplum-db/gpdb.git --tags
 
 make oss-rpm
 ci/main/scripts/verify-rpm.bash gpupgrade-*.rpm "Open Source"
